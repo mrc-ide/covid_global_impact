132a52a470be7ba69f59bd1020f7b5ac8e3399e9
CharlieWhittaker <cwhittaker1000@gmail.com>
2020-05-26 22:08:09 +0100

    triggering updates

--- 5_Suppression_Triggering_and_Exploration/Old/Final_Trigger_Running.R
+++ 5_Suppression_Triggering_and_Exploration/Old/Final_Trigger_Running.R
@@ -11,7 +11,7 @@ trigger_thresholds <- c(1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120,
                         140, 160, 180, 200, 235, 270, 300, 335, 370, 400, 450, 500,
                         600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2250, 2500,
                         2750, 3000, 3500, 4000, 4500, 5000, 6000, 7000, 8000, 10000,
-                        20000, 30000, 40000, 50000, 75000, 100000, 150000)
+                        20000, 30000, 40000, 50000, 75000, 100000)
 
 # Loading in ICU Capacity
 ICU <- income_strata_healthcare_capacity$ICU_beds
@@ -21,7 +21,7 @@ UMIC_icu <- (ICU[3] * 50000000 /1000) / 2
 HIC_icu <- (ICU[4] * 50000000 /1000) / 2
 
 # Defining Parameters Used in All Model Runs
-replicates <- 60
+replicates <- 5
 R0 <- c(3, 3)
 tt_R0 <- c(0, 50)
 suppression_reduction <- 0.25
@@ -57,9 +57,9 @@ for (i in 1:length(countries)) {
                                          ICU_bed_capacity = 10000000,
                                          income_strata_healthcare_capacity = income_strata_healthcare_capacity,
                                          poorer_outcomes = FALSE)
-    a_max_ICU_req[i, j] <- get_max_ICU_req(x)
-    a_time_in_lockdown[i, j] <- get_time_in_lockdown(x)
-    a_deaths[i, j] <- get_total_deaths(x)
+    a_max_ICU_req[i, j] <- median(get_max_ICU_req(x))
+    a_time_in_lockdown[i, j] <- median(get_time_in_lockdown(x))
+    a_deaths[i, j] <- median(get_total_deaths(x))
     print(j)
   }
 }
@@ -81,28 +81,15 @@ no_constraints_overall <- a_max_ICU %>%
   left_join(a_total_deaths, by = c("setting", "threshold")) %>%
   mutate(setting = factor(setting, levels = c("LIC", "LMIC", "UMIC", "HIC"))) %>%
   mutate(threshold = factor(threshold, levels = paste0("ICU_inc", as.integer(trigger_thresholds)))) %>%
-  group_by(setting, time_in_lockdown) %>%
-  filter(max_capacity == min(max_capacity))
+  group_by(setting, time_in_lockdown) #%>%
+  #filter(max_capacity == min(max_capacity))
 
-saveRDS(no_constraints_overall, "no_constraints_overall_df.rds")
-no_constraints_overall <- readRDS("no_constraints_overall_df.rds")
+# saveRDS(no_constraints_overall, "no_constraints_overall_df.rds")
+# no_constraints_overall <- readRDS("no_constraints_overall_df.rds")
 
-x <- no_constraints_overall %>%
-  filter(!(setting == "LIC" & threshold == "ICU_inc200"),
-         !(setting == "LIC" & threshold == "ICU_inc270"),
-         !(setting == "LIC" & threshold == "ICU_inc500"),
-         !(setting == "LIC" & threshold == "ICU_inc800"),
-         !(setting == "LMIC" & threshold == "ICU_inc335"),
-         !(setting == "LMIC" & threshold == "ICU_inc2500"),
-         !(setting == "UMIC" & threshold == "ICU_inc400"),
-         !(setting == "UMIC" & threshold == "ICU_inc800"),
-         !(setting == "UMIC" & threshold == "ICU_inc3000"),
-         !(setting == "HIC" & threshold == "ICU_inc700"))
-ggplot(x, aes(x = time_in_lockdown, y = max_capacity, col = setting)) +
-  geom_path(size = 2)
-
+x <- no_constraints_overall 
 a <- ggplot(x, aes(x = time_in_lockdown, y = max_capacity, col = setting)) +
-  geom_path(size = 2) +
+  geom_line(size = 2) +
   scale_colour_manual(labels = c("Low Income", "Lower Middle Income", "Upper Middle Income", "High Income"),
                       values = c("#B7C0EE", "#7067CF", "#362E91", "#241F60"),
                       name = "Income Strata") +
@@ -145,9 +132,9 @@ for (i in 1:length(countries)) {
                                          ICU_bed_capacity = NULL,
                                          income_strata_healthcare_capacity = income_strata_healthcare_capacity,
                                          poorer_outcomes = poorer_outcomes[i])
-    b_max_ICU_req[i, j] <- get_max_ICU_req(x)
-    b_time_in_lockdown[i, j] <- get_time_in_lockdown(x)
-    b_deaths[i, j] <- get_total_deaths(x)
+    b_max_ICU_req[i, j] <- median(get_max_ICU_req(x))
+    b_time_in_lockdown[i, j] <- median(get_time_in_lockdown(x))
+    b_deaths[i, j] <- median(get_total_deaths(x))
     print(j)
   }
 }
@@ -169,85 +156,9 @@ constraints_overall <- b_max_ICU %>%
   mutate(setting = factor(setting, levels = c("LIC_poor", "LIC", "LMIC_poor", "LMIC", "UMIC", "HIC"))) %>%
   mutate(threshold = factor(threshold, levels = paste0("ICU_inc", as.integer(trigger_thresholds)))) %>%
   group_by(setting, time_in_lockdown) %>%
-  filter(max_capacity == min(max_capacity))
+  filter(deaths == min(deaths)) 
 
-saveRDS(constraints_overall, "constraints_overall_df.rds")
-constraints_overall <- readRDS("constraints_overall_df.rds")
-
-y <- constraints_overall %>%
-  filter(!(setting == "LMIC" & threshold == "ICU_inc50"),
-         !(setting == "LMIC" & threshold == "ICU_inc120"),
-         !(setting == "LMIC" & threshold == "ICU_inc800"),
-         !(setting == "LMIC" & threshold == "ICU_inc1000"),
-         !(setting == "LMIC" & threshold == "ICU_inc2250"),
-         !(setting == "LMIC" & threshold == "ICU_inc370"),
-         !(setting == "LMIC" & threshold == "ICU_inc270"),
-         !(setting == "LMIC" & threshold == "ICU_inc500"),
-         !(setting == "LMIC" & threshold == "ICU_inc600"),
-         !(setting == "LMIC" & threshold == "ICU_inc180"),
-         !(setting == "LMIC" & threshold == "ICU_inc90"),
-         !(setting == "LIC" & threshold == "ICU_inc70"),
-         !(setting == "LIC" & threshold == "ICU_inc160"),
-         !(setting == "LIC" & threshold == "ICU_inc80"),
-         !(setting == "LIC" & threshold == "ICU_inc100"),
-         !(setting == "LIC" & threshold == "ICU_inc700"),
-         !(setting == "LIC" & threshold == "ICU_inc450"),
-         !(setting == "LIC" & threshold == "ICU_inc270"),
-         !(setting == "LIC" & threshold == "ICU_inc370"),
-         !(setting == "LIC" & threshold == "ICU_inc140"),
-         !(setting == "LIC" & threshold == "ICU_inc200"),
-         !(setting == "LIC" & threshold == "ICU_inc600"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc70"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc90"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc80"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc100"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc140"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc800"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc600"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc700"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc370"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc450"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc270"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc200"),
-         !(setting == "LIC_poor" & threshold == "ICU_inc335"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc50"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc90"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc60"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc200"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc100"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc180"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc270"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc370"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc500"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc600"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc800"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc1000"),
-         !(setting == "LMIC_poor" & threshold == "ICU_inc2250"),
-         !(setting == "UMIC" & threshold == "ICU_inc60"),
-         !(setting == "UMIC" & threshold == "ICU_inc20"),
-         !(setting == "UMIC" & threshold == "ICU_inc70"),
-         !(setting == "UMIC" & threshold == "ICU_inc1500"),
-         !(setting == "UMIC" & threshold == "ICU_inc140"),
-         !(setting == "UMIC" & threshold == "ICU_inc235"),
-         !(setting == "UMIC" & threshold == "ICU_inc370"),
-         !(setting == "UMIC" & threshold == "ICU_inc450"),
-         !(setting == "UMIC" & threshold == "ICU_inc800"),
-         !(setting == "UMIC" & threshold == "ICU_inc2000"),
-         !(setting == "UMIC" & threshold == "ICU_inc500"),
-         !(setting == "HIC" & threshold == "ICU_inc180"),
-         !(setting == "HIC" & threshold == "ICU_inc270"),
-         !(setting == "HIC" & threshold == "ICU_inc70"),
-         !(setting == "HIC" & threshold == "ICU_inc180"),
-         !(setting == "HIC" & threshold == "ICU_inc80"),
-         !(setting == "HIC" & threshold == "ICU_inc90"),
-         !(setting == "HIC" & threshold == "ICU_inc335"),
-         !(setting == "HIC" & threshold == "ICU_inc200"),
-         !(setting == "HIC" & threshold == "ICU_inc300"),
-         !(setting == "HIC" & threshold == "ICU_inc700"),
-         !(setting == "HIC" & threshold == "ICU_inc900"),
-         !(setting == "HIC" & threshold == "ICU_inc1250"),
-         !(setting == "HIC" & threshold == "ICU_inc1750"))
-
+y <- constraints_overall
 b <- ggplot(y, aes(x = time_in_lockdown, y = deaths, col = setting)) +
   geom_path(size = 2, aes(linetype = setting)) +
   scale_colour_manual(labels = c("Low Income Poor", "Low Income", "Lower Middle Income Poor",
@@ -261,3 +172,7 @@ b <- ggplot(y, aes(x = time_in_lockdown, y = deaths, col = setting)) +
   labs(y = "Total Number of Deaths", x = "Proportion of Time in Lockdown") +
   guides(linetype = "none")
 b
+
+saveRDS(constraints_overall, "constraints_overall_df.rds")
+constraints_overall <- readRDS("constraints_overall_df.rds")
+
